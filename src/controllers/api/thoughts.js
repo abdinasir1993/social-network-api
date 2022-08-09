b7d1e2206e71f913c6a4d26de2bf5fd4a1fdaf5b
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-08-09 19:50:00 +0100

    completed

--- src/controllers/api/thoughts.js
+++ src/controllers/api/thoughts.js
@@ -1,9 +1,74 @@
-const getAllThought = (res, req) => {};
-const getSingleThought = (res, req) => {};
-const createThought = (res, req) => {};
-const updateThought = (res, req) => {};
-const deleteThought = (res, req) => {};
+const { Thought } = require('../../../models');
 
+const getAllThought = async (req, res) => {
+  try {
+    const thoughts = await Thought.find({});
+    return res.json({ data: thoughts });
+  } catch (error) {
+    console.log(
+      `[ERROR]:please try again could not complete request| ${error.message}`
+    );
+  }
+};
+
+const getSingleThought = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const thought = await Thought.findById(id);
+    if (!thought) {
+      return res.status(404).json({ success: false });
+    }
+    return res.json({ data: thought });
+  } catch (error) {
+    console.log(
+      `[ERROR]:please try again could not complete request| ${error.message}`
+    );
+  }
+};
+
+const createThought = async (req, res) => {
+  try {
+    const thought = req.body;
+    await Thought.create(thought);
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(
+      `[ERROR]:please try again could not complete request| ${error.message}`
+    );
+  }
+};
+
+const updateThought = async (req, res) => {
+  try {
+    const thought = await Thought.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(
+      `[ERROR]:please try again could not complete request| ${error.message}`
+    );
+  }
+};
+
+const deleteThought = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const thought = await Thought.findById(id);
+    if (!thought) {
+      res.status(404).json({
+        message: '[ERROR]:please try again could not complete request',
+      });
+      return;
+    }
+    await Thought.deleteOne(thought);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(
+      `[ERROR]:please try again could not complete request| ${error.message}`
+    );
+  }
+};
 module.exports = {
   getAllThought,
   getSingleThought,
--- src/controllers/api/users.js
+++ src/controllers/api/users.js
@@ -1,17 +1,75 @@
-const getAllUsers = (req, res) => {
-  console.log('all users');
+const { User } = require('../../../models');
+
+const getAllUsers = async (req, res) => {
+  try {
+    const user = await User.find({});
+    return res.json({ data: user });
+  } catch (error) {
+    console.log(
+      `[ERROR]:please try again could not complete request ${error.message}`
+    );
+  }
 };
-const getSingleUser = (req, res) => {
-  console.log('1 users');
+
+const getSingleUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id).populate('thoughts');
+
+    console.log(user);
+    if (!user) {
+      return res.status(404).json({ success: false });
+    }
+    return res.json({ data: user });
+  } catch (error) {
+    console.log(
+      `[ERROR]:please try again could not complete request ${error.message}`
+    );
+  }
 };
-const createUser = (req, res) => {
-  console.log('create users');
+
+const createUser = async (req, res) => {
+  try {
+    const user = req.body;
+    await User.create(user);
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(
+      `[ERROR]:please try again could not complete request ${error.message}`
+    );
+  }
 };
-const updateUser = (req, res) => {
-  console.log('update users');
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(
+      `[ERROR]:please try again could not complete request ${error.message}`
+    );
+  }
 };
-const deleteUser = (req, res) => {
-  console.log('delete users');
+
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).json({
+        message: '[ERROR]:YOU WERE SUPPOSE TO FOLLOW THE TRAIN CJ!',
+      });
+      return;
+    }
+    await User.deleteOne(user);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(
+      `[ERROR]:please try again could not complete request ${error.message}`
+    );
+  }
 };
 
 module.exports = {
